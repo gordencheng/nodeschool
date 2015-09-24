96c5a58f6f7fe809dee78786c81781100880b675
Gorden Cheng <gorden.cheng@gmail.com>
2015-09-23 22:19:20 -0700

    completed challenge 10

---
+++ javascripting/array.js
@@ -0,0 +1,7 @@
+var pizzaToppings = [ 'tomato sauce', 'cheese', 'pepperoni' ];
+
+console.log(pizzaToppings);
+//for (var i = 0; i < pizzaToppings.length; i++)
+//{
+//  console.log(pizzaToppings[i]);
+//}
